3facab1c5ac29f9f8694e40be4643d2f89cfcf9b
izackqa <izackbh@gmail.com>
2025-01-26 13:37:14 -0300

    Update buy.cy.js
    Ajustes comentários

--- cypress/e2e/3_compras/buy.cy.js
+++ cypress/e2e/3_compras/buy.cy.js
@@ -1,6 +1,10 @@
 describe('Testes de Interação com o Carrinho - Automação Pratica', () => {
 
     it('1. Validar inclusão de item no Carrinho', () => {
+        /* Dado que o usuário recebeu o link de um produto
+           Quando o usuário realizar a compra
+           A página deverá exibir que há um novo item em seu carrinho
+        */
         cy.visitarPagina('vestidoPreto')
         cy.validarCarrinhoVazio();
         cy.adicionarCarrinho();
@@ -13,7 +17,10 @@ describe('Testes de Interação com o Carrinho - Automação Pratica', () => {
 
 
     it('2. Retirar Item do Carrinho', () => {
-
+        /* Dado que o usuário esta na página de pre-order
+           Quando houver a desistencia de compra de um item
+           Então a página permitir excluir o item do carrinho
+        */
         cy.visitarPagina('vestidoBranco');
         cy.adicionarCarrinho();
         cy.get('.cross').click();
