104ea0b0caaeda109781b1d54fabe7f13fe550b5
izack21 <izackbh@gmail.com>
2025-01-27 08:52:06 -0300

    Ajuste CI

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const {
 } = require('cypress');
 
 module.exports = defineConfig({
+  projectId: "shiw75",
   e2e: {
     baseUrl: 'http://automationpractice.pl/index.php?controller=authentication&back=my-account',
     setupNodeEvents(on, config) {
